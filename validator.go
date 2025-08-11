0a63a7d5588722e014e06b30ffff0f9fa669a7ae
SY <shiiyan79@gmail.com>
2025-08-11 12:28:30 +0900

    doc: add comment to validator

--- validator.go
+++ validator.go
@@ -1,5 +1,9 @@
+// Package govalid provides type-safe validation code generation for structs based on markers.
 package govalid
 
+// Validator interface for structs that can validate themselves.
+// This interface is implemented by generated validation code to enable
+// middleware and other consumers to validate structs polymorphically.
 type Validator interface {
 	Validate() error
 }
